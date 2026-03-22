0c9a1caeb01cbf973f9d6f745d0cbeff1d2c1b2e
akjfasdf <162512273+akjfasdf@users.noreply.github.com>
2026-03-22 15:29:13 -0700

    Update javadoc in package-info.java

--- TP1_Updated/src/entityClasses/package-info.java
+++ TP1_Updated/src/entityClasses/package-info.java
@@ -3,7 +3,8 @@
  *
  * <p> Description: This is the entityClasses package. The purpose of this package is to organize the Model classes for Post, Reply, and User information. 
  * This package demonstrates MVC in alignment with the provided GUI packages since similar to how entityClasses had a model class labeled "User.java," 
- * the additional model classes in this package are labeled based on the data they manage, such as the Post class and the Reply class. <p>
+ * the additional model classes in this package are labeled based on the data they manage, such as the Post class for post data and the Reply class for reply
+ * data . <p>
  */
 
 package entityClasses;
