411f6cd1bbda87a9b11f81aba3d4d8c9a7800243
akjfasdf <162512273+akjfasdf@users.noreply.github.com>
2026-03-22 15:21:25 -0700

    Update package-info.java

--- TP1_Updated/src/guiViewPost/package-info.java
+++ TP1_Updated/src/guiViewPost/package-info.java
@@ -1,1 +1,9 @@
+/*******
+ * <p> Title: package guiViewPost. </p>
+ *
+ * <p> Description: This is the guiViewPost package. The purpose of this package is to manage View and Controller methods for all of the CRUD operations 
+ * (Create, Read, Update, and Delete). This package demonstrates MVC in alignment similar to the provided GUI packages since, similar to provided GUI packages, 
+ * the specific MVC classes (in this case, the Controller and View classes) are explicitly labeled.  <p>
+ */
+
 package guiViewPost;
