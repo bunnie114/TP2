aab7f71541075012867069ea13f21ea5c57b74fa
akjfasdf <162512273+akjfasdf@users.noreply.github.com>
2026-03-22 15:28:11 -0700

    Updating javadoc in package-info.java

--- TP1_Updated/src/entityClasses/package-info.java
+++ TP1_Updated/src/entityClasses/package-info.java
@@ -2,9 +2,8 @@
  * <p> Title: package entityClasses. </p>
  *
  * <p> Description: This is the entityClasses package. The purpose of this package is to organize the Model classes for Post, Reply, and User information. 
- * This package demonstrates MVC in alignment with the provided GUI packages since it allows guiViewPost package to access post data so that users can view post details. 
- * MVC alignment with the provided GUI packages is also demonstrated with the guiPostForum package, since the entityClasses package shares data with class 
- * ControllerPostForum to ensure proper view navigation. <p>
+ * This package demonstrates MVC in alignment with the provided GUI packages since similar to how entityClasses had a model class labeled "User.java," 
+ * the additional model classes in this package are labeled based on the data they manage, such as the Post class and the Reply class. <p>
  */
 
 package entityClasses;
